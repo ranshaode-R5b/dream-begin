78688b78cf0f5f0554d09e129ab4fcc8395ed01f
ranshaode-R5b <690954460@qq.com>
2025-01-03 19:12:31 +0800

    Delete rna-seq directory

--- rna-seq/fea.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-cat /home/renshida/rna-seq/data/SRR_Acc_List.txt|while read id;
-do
-	featureCounts -a /home/renshida/rna-seq/data/ref/rn6.ncbiRefSeq.gtf -o /home/renshida/rna-seq/data/subread/${id}_final_counts.txt -T 8 -p /home/renshida/rna-seq/data/hisat2/trimmomatic/sort/${id}.sort.bam
-done
--- rna-seq/samtools.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-cat /home/renshida/rna-seq/data/SRR_Acc_List.txt|while read id;
-do
-        samtools sort -o /home/renshida/rna-seq/data/hisat2/${id}.sort.bam /home/renshida/rna-seq/data/hisat2/${id}.sam
-done
